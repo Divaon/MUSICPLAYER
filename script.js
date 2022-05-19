ea699a4bf3660534c36a8b4d66936e8d0a54df77
Divaon <56230474+Divaon@users.noreply.github.com>
2022-05-19 13:27:21 +0300

    1
    1

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("mydropdown").classList.toggle("show");
-}
+let allMusic = [
+  {
+    name: "Лего",
+    artist: "Ваня дмитриенко",
+    img: "DQBBAtuUEAIcZuL.jpg",
+    src: "1"
+  },
+  {
+      name: "XAGI VAGI",
+      artist: "Jason Swann, Eugene Demuckiy",
+      img: "DQBBAtuUEAIcZuL.jpg",
+      src: "2"
+  },
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  {
+      name: "ХЛОПАЙ",
+      artist: "INSTASAMKA",
+      img: "DQBBAtuUEAIcZuL.jpg",
+      src: "3"
+  },
+];
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+isMusicPaused = true;
+login = false;
+
+
+let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+
+window.addEventListener("load", ()=>{
+  loadMusic(musicIndex);
+  // playingSong(); 
+});
+
+function loadMusic(indexNumb){
+  console.log(allMusic[indexNumb - 1].name);
+  // console.log(docs );
+  // console.log(docs1 );
+  console.log(musicName );
+  console.log(musicArtist );
+  musicName.innerHTML = allMusic[indexNumb - 1].name;
+  musicArtist.innerHTML = allMusic[indexNumb - 1].artist;
+  // mainAudio.src = `songs/${allMusic[indexNumb - 1].srlkjhgfc}.mp3`;
 }
